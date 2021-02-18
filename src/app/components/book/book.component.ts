b5748cc200f1b0d2e74d842486ee46859cd5e4ba
WIN-894K2QHFTQ8\Maks <maxbeere@yandex.ru>
2021-02-18 23:07:13 +0300

    feat: add style

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Category, IBook } from 'src/app/models/book.model';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss'],
 })
-export class BookComponent implements OnInit {
-  constructor() {}
+export class BookComponent {
+  @Input() book: IBook;
 
-  ngOnInit(): void {}
+  constructor() {
+    this.book = {
+      name: 'Book',
+      description: 'string',
+      price: 2,
+      category: Category.Horror,
+      createDate: 2,
+      isAvailable: true,
+    };
+
+    // ngOnInit(): void {}
+  }
 }
