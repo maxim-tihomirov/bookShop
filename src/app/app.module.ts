1c26475914551fb0ce45ea80965f1d390087988f
WIN-894K2QHFTQ8\Maks <maxbeere@yandex.ru>
2021-02-18 22:13:26 +0300

    feat: create book component & book model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookComponent } from './components/book/book.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BookComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/models/book.model.ts
@@ -0,0 +1,15 @@
+export interface IBook {
+  name: string;
+  description: string;
+  price: number;
+  category: Category;
+  createDate: number;
+  isAvailable: boolean;
+}
+
+export const enum Category {
+  Horror = 'Horror',
+  Detective = 'Detective',
+  Fantasy = 'Fantasy',
+  Business = 'Business & Finance',
+}
